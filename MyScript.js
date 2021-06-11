e03495bc5c6890f1d5fd355f05db0fe1660c9557
d1mon1k <ostrovskiy.a.97@gmail.com>
2021-06-11 08:53:13 +0300

    second commit

--- MyScript.js
+++ MyScript.js
@@ -1,52 +1,48 @@
-const slider = document.querySelector('.slider'),
-  sliderList = slider.querySelector('.slider-list'),
-  sliderTrack = slider.querySelector('.slider-track'),
-  slides = slider.querySelectorAll('.slide'),
-  arrows = slider.querySelector('.slider-arrows');
+// let slider = document.querySelector('.slider'),
+//   sliderList = slider.querySelector('.slider-list'),
+//   sliderTrack = slider.querySelector('.slider-track'),
+//   slides = slider.querySelectorAll('.slide'),
+//   arrows = slider.querySelector('.slider-arrows');
 
-let slideIndex = 0,
-  slideWidth = slides[0].offsetWidth,
-  posX1 = 0,
-  posX2 = 0,
-  posInit = 0,
-  posFinal = 0,
-  posThreshold = slideWidth * 0.35,
-  trfRegExp = /[-0-9.]+(?=px)/; //???????
-// style = sliderTrack.style.transform = `translate3d(0px 0px 0px)`;
+// let slideIndex = 0,
+//   slideWidth = slides[0].offsetWidth,
+//   posX1 = 0,
+//   posX2 = 0,
+//   posInit = 0,
+//   posFinal = 0,
+//   posThreshold = slideWidth * 0.35,
+//   trfRegExp = /[-0-9.]+(?=px)/; //* С помощью регулярного выражения с приминением .match(ниже по коду) мы возвращаем массив трёх чисел (Потому что translate3d имеет три числа перед (?=px) и можем обратиться нарпимер к оси Х ([0]) и от него отнять posX2 и переместить наш трек)
 
-
-arrows.children[0].classList.toggle('.disabled', slideIndex === 0)
-arrows.children[0].classList.toggle('.disabled', slideIndex === slides.length - 1)
+// slider.addEventListener('touchstart', swipeStart)
+// slider.addEventListener('mousedown', swipeStart)
 
 
-slider.addEventListener('touchstart', swipeStart)
-slider.addEventListener('mousedown', swipeStart)
+// function swipeStart() {
+//   let evt = getEvent()
 
+//   //* берем начальную позицию курсора по оси Х
+//   posInit = posX1 = evt.clientX
+//   //* убираем плавный переход, чтобы track двигался за курсором без задержки
+//   //* т.к. он будет включается в функции slide()
+//   sliderTrack.style.transition = ''
 
+//   //* и сразу начинаем отслеживать другие события на документе
+//   document.addEventListener('touchmove', swipeAction)
+//   document.addEventListener('touchend', swipeEnd)
+//   document.addEventListener('mousemove', swipeAction)
+//   document.addEventListener('mouseup', swipeEnd)
+// }
 
-function swipeStart() {
-  let evt = getEvent()
+// function swipeAction() {
+//   let evt = getEvent(),
+//     style = sliderTrack.style.transform = `translate3d(0px, 0px, 0px)`,
+//     transform = +style.match(trfRegExp)[0];
 
-  //* берем начальную позицию курсора по оси Х
-  posInit = posX1 = evt.clientX
-  //* убираем плавный переход, чтобы track двигался за курсором без задержки
-  //* т.к. он будет включается в функции slide()
-  sliderTrack.style.transition = ''
+//   posX2 = posX1 - evt.clientX
+//   posX1 = evt.clientX
 
-  //* и сразу начинаем отслеживать другие события на документе
-  document.addEventListener('touchmove', swipeAction)
-  document.addEventListener('touchend', swipeEnd)
-  document.addEventListener('mousemove', swipeAction)
-  document.addEventListener('mouseup', swipeEnd)
-}
-
-function swipeAction() {
-  posX2 = posX1 - event.clientX
-  posX1 = posX2
-
-  console.log(posX2)
-
-}
+//   sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px})`
+// }
 
 function swipeEnd() {
 
@@ -55,12 +51,88 @@ function swipeEnd() {
 
 
 
+// function getEvent() {
+//   return event.type.search('touch') !== -1 ? event.touches[0] : event
+// }
+
+
+// function slide() {
+//   sliderTrack.style.transition = 'transform .5s'; //???????
+//   sliderTrack.style.transform = `translate3d(-${slideWidth * slideIndex}px 0px 0px)`
+
+
+//   arrows.children[0].classList.toggle('disabled', slideIndex === 0)
+//   arrows.children[0].classList.toggle('disabled', slideIndex === slides.length - 1)
+// }
+
+
+
+//===========================================
+
+let slider = document.querySelector('.slider'),
+  sliderList = slider.querySelector('.slider-list'),
+  sliderTrack = slider.querySelector('.slider-track'),
+  slides = slider.querySelectorAll('.slide'),
+  arrows = slider.querySelector('.slider-arrows'),
+  slideWidth = slides[0].offsetWidth,
+  slideIndex = 0,
+  posInit = 0,
+  posX1 = 0,
+  posX2 = 0,
+  posFinal = 0,
+  posThreshold = slideWidth * .35,
+  trfRegExp = /[-0-9.]+(?=px)/;
+
+
+// function slide() {
+//   sliderTrack.style.transition = 'transform .5s';
+//   sliderTrack.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
+
+//   // делаем стрелку prev недоступной на первом слайде
+//   // и доступной в остальных случаях
+//   // делаем стрелку next недоступной на последнем слайде
+//   // и доступной в остальных случаях
+//   prev.classList.toggle('disabled', slideIndex === 0);
+//   next.classList.toggle('disabled', slideIndex === --slides.length);
+// }
+
 function getEvent() {
-  return event.type.search('touch') != -1 ? event.touches[0] : event
+  return event.type.search('touch') !== -1 ? event.touches[0] : event
 }
 
-function slide() {
-  sliderTrack.style.transition = 'transform .5s'; //???????
+function swipeStart() {
+  let evt = getEvent();
 
-  sliderTrack.style.transform = `translate3d(-${slideWidth * slideIndex}px 0px 0px)`
+  // берем начальную позицию курсора по оси Х
+  posInit = posX1 = evt.clientX;
+
+  // убираем плавный переход, чтобы track двигался за курсором без задержки
+  // т.к. он будет включается в функции slide()
+  sliderTrack.style.transition = '';
+
+  // и сразу начинаем отслеживать другие события на документе
+  document.addEventListener('touchmove', swipeAction);
+  document.addEventListener('touchend', swipeEnd);
+  document.addEventListener('mousemove', swipeAction);
+  document.addEventListener('mouseup', swipeEnd);
 }
+
+function swipeAction() {
+  let evt = getEvent(),
+    // для более красивой записи возьмем в переменную текущее свойство transform
+    style = sliderTrack.style.transform = `translate3d(0px, 0px, 0px)`,
+    // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+    transform = +style.match(trfRegExp)[0];
+
+  posX2 = posX1 - evt.clientX;
+  posX1 = evt.clientX;
+
+  sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
+  // можно было бы использовать метод строк .replace():
+  // sliderTrack.style.transform = style.replace(trfRegExp, match => match - posX2);
+  // но в дальнейшем нам нужна будет текущая трансформация в переменной
+}
+
+
+slider.addEventListener('touchstart', swipeStart)
+slider.addEventListener('mousedown', swipeStart)
